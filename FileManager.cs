b2fc9f11e845efe19f5eeb6717e75c20ce61c39c
Maja Syrylak <msyrylak@gmail.com>
2018-10-11 22:51:27 +0100

    Add project files.

---
+++ FileManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace PartyAffiliationClassifier
+{
+    class FileManager
+    {
+        public static void FileReader(string filePath)
+        {
+        }
+    }
+}
